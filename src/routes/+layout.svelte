f8b254b347d2b2c290662bd54db6b19fc7cf4d09
AlpaarX <84776663+AlpaarX@users.noreply.github.com>
2023-08-12 12:12:58 +0900

    raw

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,14 @@
 <script>
 </script>
 
+<header></header>
 <slot />
+
+
+<style>
+  header {
+    height: 3rem;
+    border-bottom: solid 1px #384856;
+    margin: 0 0 1rem 0;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,86 @@
 <script>
+  import TinySegmenter from "tiny-segmenter";
+
+  let wordList = [
+    { word: "日本人", isWritten: false },
+    { word: "昼ごはん", isWritten: false },
+    { word: "食べる", isWritten: false },
+    { word: "おすし", isWritten: false },
+    { word: "ちがう", isWritten: false },
+    { word: "てんぷら", isWritten: false },
+    { word: "カレー", isWritten: false },
+    { word: "読む", isWritten: false },
+    { word: "雑誌", isWritten: false },
+    { word: "すし", isWritten: false },
+    { word: "じゃない", isWritten: false },
+    { word: "食べ物", isWritten: false },
+    { word: "わかる", isWritten: false },
+    { word: "ちょっと", isWritten: false },
+    { word: "からい", isWritten: false },
+    { word: "行く", isWritten: false },
+    { word: "いる", isWritten: false },
+    { word: "いい", isWritten: false },
+    { word: "日本", isWritten: false },
+    { word: "ほんとうに", isWritten: false },
+    { word: "そう", isWritten: false },
+    { word: "たくさん", isWritten: false },
+    { word: "思う", isWritten: false },
+    { word: "大好き", isWritten: false },
+    { word: "食べたい", isWritten: false },
+    { word: "いつか", isWritten: false },
+    { word: "来る", isWritten: false },
+    { word: "も", isWritten: false },
+    { word: "ほんとうに", isWritten: false },
+    { word: "おいしい", isWritten: false },
+  ];
+
+  const segmenter = new TinySegmenter();
   let input = "";
-  let resultText = "";
-    function segmentText() {
-    const rmaJa = new RakutenMA(model_ja);
-    rmaJa.featset = RakutenMA.default_featset_ja;
-    rmaJa.hash_func = RakutenMA.create_hash_func(15);
+  let segs = [""];
 
-    const text = input;
-
-    const convertedText = HanZenKaku.hs2fs(
-      HanZenKaku.hw2fw(HanZenKaku.h2z(text))
-    );
-    const tokens = rmaJa.tokenize(convertedText);
-
-    resultText = tokens ? RakutenMA.tokens2string(tokens) : "";
+  $: segs = segmenter.segment(input);
+  // $: commonWords = segs.filter((word) => wordList.includes(word));
+  function updateIsWritten() {
+    wordList = wordList.map((obj) => ({
+      ...obj,
+      isWritten: segs.includes(obj.word),
+    }));
   }
 </script>
 
-<svelte:head>
-  <script
-    type="text/javascript"
-    src="node_modules/rakutenma/rakutenma.js"
-    charset="UTF-8"
-  ></script>
-  <script
-    type="text/javascript"
-    src="node_modules/rakutenma/model_ja.js"
-    charset="UTF-8"
-  ></script>
-  <script
-    type="text/javascript"
-    src="node_modules/rakutenma/hanzenkaku.js"
-    charset="UTF-8"
-  ></script>
-</svelte:head>
-<main>
-  <div class="container">
-    <p id="output">{resultText}</p>
-    <button on:click={segmentText}></button>
+<div class="app">
+  <div class="word_list">
+    {#each wordList as word}
+      {#if word.isWritten}
+        <div class={"word word_true"}>{word.word}</div>
+      {:else}
+        <div class={"word"}>{word.word}</div>
+      {/if}
+    {/each}
   </div>
-</main>
+  <textarea class="input" bind:value={input} on:input={updateIsWritten} />
+</div>
+<button on:click={updateIsWritten}>Update</button>
+
+<!-- <p>Used words: {commonWords.length}</p>
+{#each commonWords as word}
+  <p>{word}</p>
+{/each} -->
+
+<style>
+  .app {
+    display: flex;
+  }
+  .word {
+    padding: 1rem;
+    border: 1px solid #384856;
+    border-radius: 0.2rem;
+  }
+  .word_true {
+    border: 1px solid #3b8b50;
+    background-color: #152715;
+  }
+  .input {
+    resize: none;
+  }
+</style>
